5cc00416434f3393e51c67431feba3b3b5c2372f
zhouen <nathanzhou@qq.com>
2014-04-29 23:01:15 +0200

    Modified spmv.c

--- spmv.c
+++ spmv.c
@@ -1,3 +1,14 @@
 //This is the source file
 
 #include "spmv.h"
+
+void ReadMatrix(ELLRMatrix aMatrix, const char aFileName[])
+{
+    int mrow = 10;
+    int mnnz = 50;
+    int mbandwidth = 5;
+
+    aMatrix.mrow = mrow;
+    aMatrix.mnnz = mnnz;
+    aMatrix.mbandwidth = mbandwidth;
+}
