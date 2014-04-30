bfa80126d0510684c675ae63c5bf9a330cebf6ae
zhouen <nathanzhou@qq.com>
2014-04-30 09:51:04 +0200

    Update tab size to 4

--- main.c
+++ main.c
@@ -5,14 +5,13 @@ int main()
     printf("Hello SpMV!\n");
 
     char VectorFile[MAXLEN];                            //! \var vector file name
-	char MatrixFile[MAXLEN];                            //! \var matrix file name
+    char MatrixFile[MAXLEN];                            //! \var matrix file name
     sprintf(MatrixFile, "%s%d", "amatrix", MSIZE);      //! \var create matrix file based on matrix size
     sprintf(VectorFile, "%s%d", "bvector",   MSIZE);    //! \var create vector file based on matrix size
 
     printf("\nMatrix file is: %s\n", MatrixFile);
     ELLRMatrix AMatrix;
     ReadMatrix(AMatrix, MatrixFile);
-
 
 	//!Free matrix memory
     FreeMatrix(AMatrix);
